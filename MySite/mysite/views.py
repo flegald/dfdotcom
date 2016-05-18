789d15465913311e75a64508fcecbd939150348a
flegald <flegal.david@gmail.com>
2016-05-17 20:01:07 -0700

    started splash page with bootstrap

--- MySite/mysite/views.py
+++ MySite/mysite/views.py
@@ -1,6 +1,10 @@
 from pyramid.view import view_config
 
 
-@view_config(route_name='home', renderer='templates/mytemplate.pt')
-def my_view(request):
-    return {'project': 'MySite'}
+
+@view_config(route_name='home',
+            renderer='templates/index.jinja2')
+def list_view(request):
+    """Show the homepage."""
+    message = "Hello"
+    return {'message': message}
