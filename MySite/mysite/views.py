9ccf41e09a54528f1a62f2c50862c67b93767173
flegald <flegal.david@gmail.com>
2016-05-18 16:06:20 -0700

    working on all main pages, portfolio page being css nasty

--- MySite/mysite/views.py
+++ MySite/mysite/views.py
@@ -1,10 +1,29 @@
 from pyramid.view import view_config
 
 
+@view_config(route_name='home', renderer='templates/index.jinja2')
+def home_view(request):
+    """Display the homepage."""
+    message = "Hello"
+    return {'message': message}
 
-@view_config(route_name='home',
-            renderer='templates/index.jinja2')
-def list_view(request):
-    """Show the homepage."""
+
+@view_config(route_name='portfolio', renderer='templates/portfolio.jinja2')
+def portfolio_view(request):
+    """Display portfolio."""
+    message = "Hello"
+    return {'message': message}
+
+
+@view_config(route_name='about', renderer='templates/about.jinja2')
+def about_view(request):
+    """Display About Me."""
+    message = "Hello"
+    return {'message': message}
+
+
+@view_config(route_name='contact', renderer='templates/contact.jinja2')
+def contact_view(request):
+    """Display Contact."""
     message = "Hello"
     return {'message': message}
